2f77e507f23c2187e679ab74d6063297122fe548
akabynda <st084331@student.spbu.ru>
2024-04-19 15:24:58 +0300

    EDA

---
+++ projects/task06_unsupervised_learning/init.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+file_path = 'celltocellholdout.csv'
+data = pd.read_csv(file_path).dropna()
+
+data_head = data.head()
+data_info = data.info()
+data_description = data.describe()
+
+print(data_head, data_info, data_description)
